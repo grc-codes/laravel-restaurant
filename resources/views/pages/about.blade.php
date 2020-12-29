7b853886d6063c8b59d980bc4597ce14683b7a3e
Andrew Garcia <grc.codes@gmail.com>
2020-12-28 20:33:17 -0500

    made layouts responsive for mobile and tablet

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="welcome-jumbo">
-        <h1>About Page</h1>
+    <div id='about-page'>
+        <div class='content-box'>
+            <div class='row'>
+                <div class='col-md-6'>
+                    <h1>About Page</h1>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae voluptas natus deserunt! Dolor sed atque deserunt eos cumque voluptates nam quidem id numquam. Similique, voluptas cum quibusdam quam explicabo fuga!
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolore quam similique inventore exercitationem eos optio nesciunt, laudantium dolorum quaerat consequatur ipsam perferendis, perspiciatis, harum expedita laboriosam adipisci ad accusantium.
+                    </p>
+                </div>
+                <div class='col-md-6'>
+                    <img class='img-fluid' src='/img/about-page-image.jpg' />
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/pages/giftcards.blade.php
+++ resources/views/pages/giftcards.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="welcome-jumbo">
-        <h1>Gift Cards Page</h1>
-    </div>
+    <h1>Gift Cards Page</h1>
 @endsection
