a45169d19bbfc167453a24c1cbfc589abad67d9c
Andrew Garcia <grc.codes@gmail.com>
2020-12-30 12:29:29 -0500

    added create and edit food categories pages

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,14 @@ Route::get('/admin/food-categories', function () {
     return view('admin/food-categories/all');
 });
 
+Route::get('/admin/food-categories/create', function () {
+    return view('admin/food-categories/create');
+});
+
+Route::get('/admin/food-categories/{id}/edit', function () {
+    return view('admin/food-categories/edit');
+});
+
 Route::get('/menu', function () {
     return view('menu/index');
 });
